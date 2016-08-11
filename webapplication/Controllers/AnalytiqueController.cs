4d0e8e3f890238f092c64b8f7d63d3a7adb26ef6
Peter Parker <pham.huyson87@gmail.com>
2016-08-11 07:33:09 -0400

    Prototype avec l'angle de Cobb et l'age

--- webapplication/Controllers/AnalytiqueController.cs
+++ webapplication/Controllers/AnalytiqueController.cs
@@ -4,6 +4,7 @@ using MathNet.Numerics.Statistics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 
 namespace WebApplication.Controllers
@@ -115,6 +116,197 @@ namespace WebApplication.Controllers
             return View("Index");
         }
 
+        public ActionResult AnalyzeData(int? diagnosis, int? gender, int? studyVariable, string submitButton)
+        {
+            RedirectToAction("ReportToExcel", new { var1 = "test" });
+
+            bool isAISDiagnose = diagnosis == 1;
+        
+            bool isAgeField = false;
+            bool isCobbAngleField = false;
+
+            bool isExcelOperation = submitButton.ToLower() == "excel";
+
+            var participantWithDiagnosisList = db.Diagnoses
+                                                    .Single(e => e.Id == diagnosis)
+                                                    .Participants;
+
+            if (gender.HasValue) {
+                if (gender.Value == 1) // Homme
+                {
+                    participantWithDiagnosisList = participantWithDiagnosisList
+                                                        .Where(e => e.IsMale).ToList();
+                }
+                else if (gender.Value == 2) // Femme
+                {
+                    participantWithDiagnosisList = participantWithDiagnosisList
+                                    .Where(e => !e.IsMale).ToList();
+                }
+            }
+
+            if (isAISDiagnose)
+            {
+
+                isAgeField = studyVariable == 1;
+                isCobbAngleField = studyVariable == 2;
+
+                if (isAgeField)
+                {
+
+                    var participantAgeList = participantWithDiagnosisList
+                                                .Where(p => p.DOB.HasValue)
+                                                .Select(p => (double)(DateTime.Now.Year - p.DOB.Value.Year));
+
+                    var statistics = new DescriptiveStatistics(participantAgeList);
+
+                    ViewBag.analyzeStatMin = statistics.Minimum;
+                    ViewBag.analyzeStatMax = statistics.Maximum;
+                    ViewBag.analyzeStatMedian = Statistics.Median(participantAgeList);
+                    ViewBag.analyzeStatMean = statistics.Mean;
+                    ViewBag.analyzeStatStandardDeviation = statistics.StandardDeviation;
+                    ViewBag.analyzeStatVariance = statistics.Variance;
+                    ViewBag.analyzeStatSampleSize = participantAgeList.Count();
+
+                    ViewBag.analyzeStatComputed = true;
+
+                    if (isExcelOperation) {
+
+                        var CSVContent = participantWithDiagnosisList
+                                                .Where(p => p.DOB.HasValue)
+                                                .Select(p => new {
+                                                    
+                                                    Id = p.Id,
+                                                    FirstName = p.FirstName,
+                                                    LastName = p.LastName,
+                                                    DOB = p.DOB.Value,
+                                                    Age = (double)(DateTime.Now.Year - p.DOB.Value.Year)
+                                                });
+
+                        StringBuilder sb = new StringBuilder();
+
+                        sb.Append("Id," );
+                        sb.Append("FirstName,");
+                        sb.Append("LastName,");
+                        sb.Append("DOB,");
+                        sb.Append("Age,");
+
+                        sb.Append("\r\n");
+
+                        foreach (var item in CSVContent) {
+                            sb.Append(item.Id.ToString() + ',');
+                            sb.Append(item.FirstName + ',');
+                            sb.Append(item.LastName + ',');
+                            sb.Append(item.DOB.ToString() + ',');
+                            sb.Append(item.Age.ToString() + ',');
+
+                            sb.Append("\r\n");
+                        }
+
+                        Response.Clear();
+                        Response.Buffer = true;
+                        Response.AddHeader("content-disposition", "attachment;filename=SqlExport.csv");
+                        Response.Charset = "";
+                        Response.ContentType = "application/text";
+                        Response.Output.Write(sb.ToString());
+                        Response.Flush();
+                        Response.End();
+                    }
+                }
+
+                if (isCobbAngleField)
+                {
+                    var result = from p in participantWithDiagnosisList
+                                 join a in db.Appointments
+                                    on p.Id equals a.Participant_Id
+                                 join c in db.Cobbs
+                                    on a.Id equals c.Appointment_Id
+                                 where c.Angle.HasValue
+                                 select new
+                                 {
+                                     Id = p.Id,
+                                     Angle = c.Angle,
+                                     FirstName = p.FirstName,
+                                     LastName = p.LastName,
+                                     DOB = p.DOB.Value,
+                                     Age = (double)(DateTime.Now.Year - p.DOB.Value.Year)
+                                 };
+
+                    result = result.ToList();
+
+                    var groupResult = result
+                                .GroupBy(e => e.Id)
+                                .Select(e =>
+                                    new {
+                                        Id = e.Select(f => f.Id).First(),
+                                        Angle = (double) e.Max(x => x.Angle)
+                                    }
+                                );
+
+                    var participantCobbAngleList = groupResult.Select(e => e.Angle).ToList();
+
+                    var statistics = new DescriptiveStatistics(participantCobbAngleList);
+
+                    ViewBag.analyzeStatMin = statistics.Minimum;
+                    ViewBag.analyzeStatMax = statistics.Maximum;
+                    ViewBag.analyzeStatMedian = Statistics.Median(participantCobbAngleList);
+                    ViewBag.analyzeStatMean = statistics.Mean;
+                    ViewBag.analyzeStatStandardDeviation = statistics.StandardDeviation;
+                    ViewBag.analyzeStatVariance = statistics.Variance;
+                    ViewBag.analyzeStatSampleSize = participantCobbAngleList.Count();
+
+                    ViewBag.analyzeStatComputed = true;
+
+                    if (isExcelOperation)
+                    {
+                        var CSVContent = result;
+
+                        StringBuilder sb = new StringBuilder();
+
+                        sb.Append("Id,");
+                        sb.Append("FirstName,");
+                        sb.Append("LastName,");
+                        sb.Append("DOB,");
+                        sb.Append("Age,");
+                        sb.Append("Cobb Angle,");
+
+                        sb.Append("\r\n");
+
+                        foreach (var item in CSVContent)
+                        {
+                            sb.Append(item.Id.ToString() + ',');
+                            sb.Append(item.FirstName + ',');
+                            sb.Append(item.LastName + ',');
+                            sb.Append(item.DOB.ToString() + ',');
+                            sb.Append(item.Age.ToString() + ',');
+                            sb.Append(item.Angle.ToString() + ",");
+
+                            sb.Append("\r\n");
+                        }
+
+                        Response.Clear();
+                        Response.Buffer = true;
+                        Response.AddHeader("content-disposition", "attachment;filename=SqlExport.csv");
+                        Response.Charset = "";
+                        Response.ContentType = "application/text";
+                        Response.Output.Write(sb.ToString());
+                        Response.Flush();
+                        Response.End();
+                    }
+                }
+
+            }
+
+            ViewBag.AverageCobbComputed = false;
+            ViewBag.descriptivesStatsComputed = false;
+
+            return View("Index");
+        }
+
+        public void ReportToExcel(string var1)
+        {
+
+        }
+
         #region Ajax call (JSON)
 
         public ActionResult GetData()
@@ -146,7 +338,7 @@ namespace WebApplication.Controllers
 
             return Json(cobbs, JsonRequestBehavior.AllowGet);
         }
-        
+
         #endregion
     }
 }
